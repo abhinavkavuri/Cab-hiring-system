4c46e7998b3bd6ccdae443e86a030c6846d1439e
kavuri abhinav <abhinavkavuri@gmail.com>
2018-05-11 15:54:34 +0530

    Add files via upload

--- promo_submit.php
+++ promo_submit.php
@@ -6,19 +6,31 @@ if (!isset($_SESSION['email'])) {
     
 }
 
+if(!isset($_SESSION['pcode'])){
+    $_SESSION['pcode'] = 'null';
+    
+}
+
 $pcode = $_POST['pcode'];
 $pcode = mysqli_real_escape_string($con, $pcode);
+$pc=$_SESSION['pcode'];
 
 // Query checks if the email and password are present in the database.
 $query = "SELECT pcode FROM promotion WHERE pcode='" . $pcode . "' ";
 $result = mysqli_query($con, $query)or die($mysqli_error($con));
 $num = mysqli_num_rows($result);
 // If the email and password are not present in the database, the mysqli_num_rows returns 0, it is assigned to $num.
-if ($num == 0) {
+if($pc == $pcode){
+    echo '<script type="text/javascript"> alert("Promo code already applied...cannot apply another one"); window.location.href = "/cabv2/promo_action.php"; </script>';
+    
+}
+elseif ($num == 0) {
   echo '<script type="text/javascript"> alert("Enter valid Promo code"); window.location.href = "/cabv2/promo_action.php"; </script>';
+  $_SESSION['pcode'] = "null";
 } 
 else { 
    echo '<script type="text/javascript"> alert("Promo code applied !"); window.location.href = "/cabv2/login.php"; </script>'; 
+   $_SESSION['pcode'] = $pcode;
   //$row = mysqli_fetch_array($result);
   //$_SESSION['email'] = $row['email'];
   //$_SESSION['user_id'] = $row['id'];
